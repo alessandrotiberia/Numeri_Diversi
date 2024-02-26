50b84570f28f322545e194b9a19aa45927c53ae6
alessandrotiberia <150442623+alessandrotiberia@users.noreply.github.com>
2024-02-26 17:10:58 +0100

    first commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+public static void main(String[] args) {
+    /*Scrivi un programma Java che prende due numeri da tastiera e stampa 
+    “I numeri inseriti sono uguali” se i due numeri sono uguali e viceversa stampa “I numeri inseriti sono diversi”. */
+
+        int n1=10;
+        int n2=5;
+        if (n1==n2) {
+            System.out.println("i due numeri sono uguali"); 
+        }else 
+        System.out.println("i due numeri sono diversi");
+
+
+
+
+}
+    
+}
